5048aa6ceff47b4c3cede9b5643901c0565b04a2
ThiagoZambelli <thiagozamoliveira@gmail.com>
2023-02-05 19:43:10 -0300

    Exucção do curso até a aula 3, aprendendo so requisição, JSON, metodos then  e catch e tratamento de erro

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# js-consumindo-dados-api
+<h1>Projeto executado para o curso *js-consumindo-dados-api da Alura</h1>
+
+
 Repositório do treinamento "JavaScript: Consumindo e tratando dados de uma API" da Alura Cursos Online.
+
+# -> Assuntos tratados no projeto:
+
+* Assincronicidade em JS
+* Consumo de API
+* JSON
+* Compreender o funcionamento das promises
---
+++ main.js
@@ -0,0 +1,13 @@
+let consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
+.then(resposta => resposta.json())
+.then(r => {
+    if (r.erro) {
+        throw Error('Esse CEP não existe')
+    }else{
+        console.log(r)
+    }
+    })
+.catch(erro => console.log(erro))
+.finally(mensagem => console.log('Processamento Concluido'));
+
+console.log(consultaCEP);
