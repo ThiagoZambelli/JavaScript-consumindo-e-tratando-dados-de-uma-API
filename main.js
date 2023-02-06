6cfb07cb6f1ba6f0212feafd7f195d5e37a6eaba
ThiagoZambelli <thiagozamoliveira@gmail.com>
2023-02-06 19:05:29 -0300

    Aplicação de async/await e try/catch.

--- main.js
+++ main.js
@@ -1,13 +1,40 @@
-let consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
-.then(resposta => resposta.json())
-.then(r => {
-    if (r.erro) {
-        throw Error('Esse CEP não existe')
-    }else{
-        console.log(r)
-    }
-    })
-.catch(erro => console.log(erro))
-.finally(mensagem => console.log('Processamento Concluido'));
+// let consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
+// .then(resposta => resposta.json())
+// .then(r => {
+//     if (r.erro) {
+//         throw Error('Esse CEP não existe')
+//     }else{
+//         console.log(r)
+//     }
+//     })
+// .catch(erro => console.log(erro))
+// .finally(mensagem => console.log('Processamento Concluido'));
 
-console.log(consultaCEP);
+// console.log(consultaCEP);
+
+async function buscaCep (numero){
+    try{
+        let consultaCEP = await fetch(`https://viacep.com.br/ws/${numero}/json/`);
+        let consultaCEPConvertido = await consultaCEP.json();
+        if(consultaCEPConvertido.erro){
+            throw Error ('CEP não existente!')
+        }
+        var cidade =document.getElementById('cidade');
+        var logradouro = document.getElementById('endereco');
+        var estado = document.getElementById('estado');
+        var bairro = document.getElementById('bairro');
+
+        cidade.value = consultaCEPConvertido.localidade;
+        logradouro.value = consultaCEPConvertido.logradouro;
+        estado.value = consultaCEPConvertido.uf;
+        bairro.value = consultaCEPConvertido.bairro;
+
+        console.log(consultaCEPConvertido)
+        return consultaCEPConvertido;
+    }catch (erro){
+        console.log(erro);
+    }
+}
+
+let cep = document.getElementById('cep');
+cep.addEventListener("focusout", ()=> buscaCep(cep.value));
